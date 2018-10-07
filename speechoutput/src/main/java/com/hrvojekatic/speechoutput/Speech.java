06f6a626238c7c50e8699c3d139f61e3ce475349
hkatic <hrvojekatic@gmail.com>
2018-10-07 13:37:47 +0200

    Add Javadoc and nullability checks.

--- speechoutput/src/main/java/com/hrvojekatic/speechoutput/Speech.java
+++ speechoutput/src/main/java/com/hrvojekatic/speechoutput/Speech.java
@@ -7,11 +7,21 @@ import android.util.Log;
 
 import java.util.Locale;
 
+/**
+ * A speech class for easier handling of text-to-speech in Android.
+ */
 public final class Speech {
 	private static final String TAG = Speech.class.getSimpleName();
 	private static final String DEFAULT_INIT_MESSAGE = "TextToSpeech ready.";
 	private static TextToSpeech sTts;
 
+	/**
+	 * Creates TextToSpeech object instance and performs check if something went wrong.
+	 *
+	 * @param context     An application or activity context
+	 * @param locale      A language locale to use, default is Locale.US
+	 * @param initMessage A message to speak when TextToSpeech is successfully initialized
+	 */
 	public static void create(final Context context, final Locale locale, final String initMessage) {
 		sTts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
 			@Override
@@ -29,18 +39,39 @@ public final class Speech {
 		});
 	}
 
+	/**
+	 * Creates TextToSpeech object instance and performs check if something went wrong.
+	 *
+	 * @param context     An application or activity context
+	 * @param initMessage A message to speak when TextToSpeech is successfully initialized
+	 */
 	public static void create(final Context context, final String initMessage) {
 		create(context, Locale.US, initMessage);
 	}
 
+	/**
+	 * Creates TextToSpeech object instance and performs check if something went wrong.
+	 *
+	 * @param context An application or activity context
+	 * @param locale  A language locale to use, default is Locale.US
+	 */
 	public static void create(final Context context, final Locale locale) {
 		create(context, locale, DEFAULT_INIT_MESSAGE);
 	}
 
+	/**
+	 * Creates TextToSpeech object instance and performs check if something went wrong.
+	 *
+	 * @param context An application or activity context
+	 */
 	public static void create(final Context context) {
 		create(context, Locale.US, DEFAULT_INIT_MESSAGE);
 	}
 
+	/**
+	 * Destroys TextToSpeech object to clean up resources.
+	 * Use this on onDestroy method.
+	 */
 	public static void destroy() {
 		if (sTts != null) {
 			sTts.stop();
@@ -48,15 +79,31 @@ public final class Speech {
 		}
 	}
 
+	/**
+	 * Get TextToSpeech object instance, useful for additional operations on TextToSpeech.
+	 *
+	 * @return TextToSpeech instance
+	 */
 	public static TextToSpeech getTts() {
-		return sTts;
+		if (sTts != null) {
+			return sTts;
+		} else {
+			return null;
+		}
 	}
 
+	/**
+	 * Speaks given text.
+	 *
+	 * @param text A text to speak
+	 */
 	public static void speak(String text) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			sTts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-		} else {
-			sTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+		if (sTts != null) {
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+				sTts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+			} else {
+				sTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+			}
 		}
 	}
 }
